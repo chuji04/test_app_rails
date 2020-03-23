cf48df20b3188f1410078b1e187a9250e97b48c7
churei <k.churei@gmail.com>
2020-03-23 10:48:02 +0000

    sign-up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   
+  resources :users
   get  '/signup',  to: 'users#new'
   get  '/help',    to: 'static_pages#help'    #get 'static_pages/help'
   get  '/about',   to: 'static_pages#about'
